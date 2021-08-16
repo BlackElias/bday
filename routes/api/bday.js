a10ba4c231d418f66c312fc9c10b2f99f8db0831
BlackElias <valienne.elias@outlook.com>
2021-08-16 16:23:15 +0200

    added route api

---
+++ routes/api/bday.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const router = express.Router();
+const coinsController = require('../../../controllers/api/v1/bday');
+
+/* /api/v1/coins */
+
+router.get("/", coinsController.getAll);
+router.post("/",coinsController.create);
+
+module.exports = router;
